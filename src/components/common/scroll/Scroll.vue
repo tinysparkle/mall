c8db852d1119a62aa31252ea45e070aec121ac95
tinyspark <15837553181@163.com>
2020-04-22 18:03:29 +0800

    实现下拉加载更多

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -8,17 +8,22 @@
 
 <script>
 import BScroll from "better-scroll"
+
+import {getHomeGoods} from "network/home"
 export default {
   name:"scroll",
   props:{
     probeType:{
       type:Number,
-      default:0
-    }
+      default:0,  
+    },
+   pullUpLoad:false
   },
   data () {
     return {
-      scroll:null
+      scroll:null,      
+      currentPage:1,
+      goods:[]
     }
   },
   components: {
@@ -31,16 +36,23 @@ export default {
       // 侦听类型
       probeType:this.probeType,
       // 开启上拉加载
-      pullUpLoad:true
+      pullUpLoad:this.pullUpLoad
     }),
     this.scroll.on("scroll",(position) => {
       // console.log(position);
       this.$emit("showPosition",position) 
-    })
+    }),
+    // 监听上拉事件
+    this.scroll.on("pullingUp",() => {
+      this.$emit("pullingUp")
+      })
   },
   methods: {
     backTop(x,y,time){
       this.scroll.scrollTo(x,y,time)
+    },
+    finishPullUp(){
+      this.scroll.finishPullUp()
     }
   }
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,15 +3,22 @@
     <NavBar class="home-nav">
       <h3 slot="center">购物街</h3>
     </NavBar>
-  <Scroll class="content" ref="scroll" :probeType="3" @showPosition = "showPosition">
-    <HomeSwiper :banners="banners" />
-    <Recommend :recommends="recommends" />
-    <Feature />
-    <TabControl :titles="['流行','新款','精选']" @tabClick="tabClick" class="tab-control"/>
-    <GoodsList :goods="showGoods" />
-  </Scroll>
+    <Scroll
+      class="content"
+      ref="scroll"
+      :probeType="3"
+      :pullUpLoad="true"
+      @showPosition="showPosition"
+      @pullingUp="loadMore"
+    >
+      <HomeSwiper :banners="banners" />
+      <Recommend :recommends="recommends" />
+      <Feature />
+      <TabControl :titles="['流行','新款','精选']" @tabClick="tabClick" class="tab-control" />
+      <GoodsList :goods="showGoods" />
+    </Scroll>
 
-  <BackTop  @click.native="backClick" v-show="isShow"/>
+    <BackTop @click.native="backClick" v-show="isShow" />
   </div>
 </template>
 
@@ -21,7 +28,7 @@ import Scroll from "components/common/scroll/Scroll";
 import BackTop from "components/content/backTop/BackTop";
 
 import TabControl from "components/content/tabControl/TabControl";
-import GoodsList from "components/content/goods/GoodsList"
+import GoodsList from "components/content/goods/GoodsList";
 //
 import HomeSwiper from "./childCpns/HomeSwiper";
 import Recommend from "./childCpns/Recommend";
@@ -39,8 +46,8 @@ export default {
         new: { page: 0, list: [] },
         sell: { page: 0, list: [] }
       },
-      currentGoods:"pop",
-      isShow : false
+      currentGoods: "pop",
+      isShow: false
     };
   },
   components: {
@@ -55,36 +62,46 @@ export default {
   },
   created() {
     // 请求banner图和推荐图数据
-    this.getMultiData()
+    this.getMultiData();
     // 请求商品数据
-    this.getHomeGoods('pop')
-    this.getHomeGoods('new')
-    this.getHomeGoods('sell')
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
   methods: {
-    tabClick(index){
+    // tabbar点击切换
+    tabClick(index) {
       // console.log(index);
-      switch(index){
+      switch (index) {
         case 0:
-          this.currentGoods = "pop"
-          break
+          this.currentGoods = "pop";
+          break;
         case 1:
-          this.currentGoods = "new"
-          break
+          this.currentGoods = "new";
+          break;
         case 2:
-          this.currentGoods = "sell"
-          break
+          this.currentGoods = "sell";
+          break;
       }
     },
-    backClick(){   
-      this.$refs.scroll.backTop(0,0,500)
+    // 回到顶部
+    backClick() {
+      this.$refs.scroll.backTop(0, 0, 500);
     },
-    showPosition(position){
-      this.isShow = (-position.y)>1000
+    showPosition(position) {
+      this.isShow = -position.y > 1000;
+    },
+    // 加载更多数据
+    loadMore() {
+      this.getHomeGoods(this.currentGoods);
+      setTimeout(() => {
+        this.$refs.scroll.finishPullUp();
+      }, 2000);
     },
     /**
-     * 网友请求有关的方法
+     * 网络请求有关的方法
      */
+    // 获取banners和recommend图
     getMultiData() {
       getMultiData().then(res => {
         //  console.log(res)
@@ -92,18 +109,19 @@ export default {
         this.recommends = res.data.recommend.list;
       });
     },
+    // 获取商品图片
     getHomeGoods(type) {
-      const page = this.goods[type].page + 1
+      const page = this.goods[type].page + 1;
       getHomeGoods(type, page).then(res => {
-        // console.log(res);  
-        this.goods[type].list.push(...res.data.list)
-        this.goods[type].page += 1      
+        // console.log(res);
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
       });
     }
   },
   computed: {
-    showGoods(){
-      return this.goods[this.currentGoods].list
+    showGoods() {
+      return this.goods[this.currentGoods].list;
     }
   }
 };
